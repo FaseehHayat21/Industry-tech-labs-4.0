6584798df87b21b1be7355068431d2ceacf5ea0a
quratulain08 <anniebangash497@gmail.com>
2024-11-17 07:03:40 -0800

    error fixed

--- src/component/AboutUs/InnovationBlog/InnovationBlog.jsx
+++ src/component/AboutUs/InnovationBlog/InnovationBlog.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './InnovationBlog.css';
-import aiOrganization from '../../../assets/scalingAI.png';
+import aiOrganization from '../../../assets/ScalingAI.png';
 import ecommerce3D from '../../../assets/ModellingEffect.png';
 
 const InnovationBlog = () => {
