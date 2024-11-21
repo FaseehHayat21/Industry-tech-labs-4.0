aac140c4f05a7474c8315e24d84779ea9bdaa8d0
quratulain08 <anniebangash497@gmail.com>
2024-11-21 09:17:44 -0800

    team css modified

--- src/component/AboutUs/Team/Team.jsx
+++ src/component/AboutUs/Team/Team.jsx
@@ -4,6 +4,7 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 import sirazhar from "../../../assets/sirazhar.jpg";
 import sirmansoor from "../../../assets/sirmansoor.jpg";
+import { Link } from "react-router-dom";
 
 export default function Team() {
   useEffect(() => {
@@ -12,7 +13,6 @@ export default function Team() {
 
   return (
     <>
-      {/* Wrapper with React fragment */}
       <section className="founders-section">
         <div className="banner">
           <h1>Meet Our Team</h1>
@@ -23,21 +23,31 @@ export default function Team() {
           <div className="team-members" data-aos="fade-up">
             <div className="team-member">
               <img src={sirazhar} alt="Founder" />
+
+              <Link to="/about/azhar" className="more-button">More about me</Link>
               <h3>Azhar Naeem</h3>
               <p>(Founder)</p>
               <div className="contact-info">
                 <p>0333-1234567</p>
-                <a href="mailto:azhar.naeem@example.com" className="email">azhar.naeem@example.com</a>
+                <a href="mailto:azhar.naeem@example.com" className="email">
+                  azhar.naeem@example.com
+                </a>
               </div>
+
             </div>
             <div className="team-member">
               <img src={sirmansoor} alt="Co-Founder" />
+              <Link to="/about/mansoor" className="more-button">More about me</Link>
               <h3>Mansoor Ahsan</h3>
               <p>(Co-Founder)</p>
               <div className="contact-info">
                 <p>0300-1234567</p>
-                <a href="mailto:mansoor.ahsan@example.com" className="email">mansoorahsan@example.com</a>
+                <a href="mailto:mansoor.ahsan@example.com" className="email">
+                  mansoorahsan@example.com
+                </a>
               </div>
+
+
             </div>
           </div>
         </div>
