ecb1ba7b70f374ce36dc41d82e87e83633693d7a
quratulain08 <anniebangash497@gmail.com>
2024-11-25 10:01:47 -0800

    Upcoming-events done

--- src/component/Community/Founder/Founders.jsx
+++ src/component/Community/Founder/Founders.jsx
@@ -35,7 +35,36 @@ export default function Founders() {
           stake in their success.
             </p>
       </div>
-      
+
+       <div className="upcoming-events">
+        <h2>Upcoming Events</h2>
+        <div className="events">
+          <div className="event-card">
+            {/*  <h3>Event 1</h3> */} 
+            {/* <p><strong>Date:</strong> November 21, 2024</p> */}
+            {/* <p><strong>Location:</strong> 10 Fan Pier Blvd, Boston, MA 02210</p> */}
+            <p><strong>Coming Soon!</strong></p>
+            {/*<a href="#">LEARN MORE</a> */} 
+          </div>
+          <div className="event-card">
+            {/*  <h3>Event 2</h3> */} 
+            {/* <p><strong>Date:</strong> November 19, 2024</p> */}
+            {/* <p><strong>Location:</strong> 10 Fan Pier Blvd, Boston, MA 02210</p> */}
+            <p><strong>Coming Soon!</strong></p>
+           {/*<a href="#">LEARN MORE</a> */} 
+          </div>
+          <div className="event-card">
+           {/*  <h3>Event 3</h3> */} 
+            {/* <p><strong>Date:</strong> November 12, 2024</p> */}
+            {/* <p><strong>Location:</strong> 10 Fan Pier Blvd, Boston, MA 02210</p> */}
+            <p><strong>Coming Soon!</strong></p>
+             {/*<a href="#">LEARN MORE</a> */} 
+          </div>
+        </div>
+        <div className="view-all">
+          <a href="#">VIEW ALL EVENTS</a>
+        </div>
+      </div>
     </section>
   );
 }
