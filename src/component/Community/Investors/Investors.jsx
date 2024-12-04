e8afddd5a96f3c3b56106b0d7002023c3d30aea8
quratulain08 <141954239+quratulain08@users.noreply.github.com>
2024-12-04 20:06:09 +0500

    Community content change

--- src/component/Community/Investors/Investors.jsx
+++ src/component/Community/Investors/Investors.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import './Investors.css';
 
@@ -9,15 +8,29 @@ export default function Investors() {
         <h1>Investors</h1>
       </div>
       <div className="content">
-      <h1><strong>Become an Infinity Tech Labs Expert</strong></h1>
-        <p>Access cutting-edge technology and passionate entrepreneurs across the full Infinity Tech Labs portfolio, featuring startups, founders, and dedicated team members.
-</p>
         <p>
-     
-The ITL Investor Network connects accredited investors with ITL startups and alumni who are actively raising funds. Investors in the network receive monthly portfolio updates during the final week of each month.
+          Gain access to novel, technology-driven entrepreneurs from the entire Infinity Labs portfolio, which includes startups, founders, and team members.
+        </p>
+        <p>
+          The Infinity Labs Investor Network is designed to connect accredited investors with startups and alumni that will be fundraising. All investors in the network will receive monthly portfolio emails during the last week of each month.
         </p>
       </div>
-    
+
+      <div className="content">
+        <h1><strong>An Active Infinity Labs Community Member</strong></h1>
+        <p>
+          There are several more benefits to joining the Infinity Labs Investor Network:
+        </p>
+        <h2>Event Partnerships</h2>
+        <p>
+          Infinity Labs holds a number of in-person or virtual events throughout the year. These events include networking engagements, thought leadership talks, fireside chats and panels, and exhibition opportunities.
+        </p>
+
+        <h2>Ecosystem Access</h2>
+        <p>
+          Engaged investors will receive an invitation to participate in market access programs and ecosystem events where Infinity Labs has a strong presence throughout the year. These community events have the focused goal of enabling startups to network with key stakeholders within our physical communities and where we have clusters of active alumni, investors, and community partners.
+        </p>
+      </div>
     </section>
   );
 }
--- src/component/Community/Joins/Joins.jsx
+++ src/component/Community/Joins/Joins.jsx
@@ -3,18 +3,45 @@ import './Joins.css';
 
 export default function Joins() {
   return (
-    <section className="founders-section">
+    <section className="innovation-network-section">
       <div className="banner">
         <h1>Become part of the Innovation Network</h1>
       </div>
+
       <div className="content">
-      <h1><strong>Let’s Join Hands</strong></h1>
-        <p>Advance Infinity Tech Labs’ mission. Empower visionary founders to challenge the norm and drive lasting change. As a larger network, we remain committed to supporting founders and delivering wide-reaching societal benefits through the power of tech-entrepreneurship.</p>
+        <h2><strong>Let’s Join Hands</strong></h2>
         <p>
-        There are several ways you can support our mission of empowering startups and transforming industries:
+          Advance Infinity Tech Labs’ mission. Empower visionary founders to challenge the norm and drive lasting change. As a larger network, we remain committed to supporting founders and delivering wide-reaching societal benefits through the power of tech-entrepreneurship.
         </p>
       </div>
-     
+
+      <div className="content">
+        <h2>Join the Infinity Tech Labs Community</h2>
+        <p>
+          Introduce yourself and stay updated on program highlights, important dates, expert insights, and partnership opportunities.
+        </p>
+      </div>
+
+      <div className="content">
+        <h2>A Note From CEO</h2>
+        <p>
+          At Infinity Tech Labs, we are shaping the future by fostering innovation, empowering founders, and creating a thriving ecosystem that bridges the gap between local potential and global possibilities. Our mission is to build a dynamic network that connects visionary entrepreneurs, industry leaders, and academic pioneers, enabling access to resources, mentorship, and opportunities at every step of their journey to realize their ambitions, scale their impact, and contribute to a better tomorrow.
+        </p>
+      </div>
+
+      <div className="content">
+        <h2>A National Network of Innovators</h2>
+        <p>
+          Our ecosystem involves multiple stakeholders which are connected by the Infinity Tech Labs. Find yourself to fit in one of the roles. You can learn more about additional ways to join our network.
+        </p>
+      </div>
+
+      <div className="content">
+        <h2>Join the Infinity Tech Labs Network</h2>
+        <p>
+          Not sure where you fit? Introduce yourself to explore the various roles at Infinity Tech Labs, such as becoming a mentor or expert, discovering the accelerator programs that suit you best, or exploring partnership opportunities.
+        </p>
+      </div>
     </section>
   );
 }
